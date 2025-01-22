6ef763defa2b3d914e1ea69107c2c2b0772431e9
davidhjensen <dhjensen02@gmail.com>
2025-01-22 10:47:18 -0700

    initial azure setup and form parsing using request.formData()

--- azure/src/functions/techpackRequest.js
+++ azure/src/functions/techpackRequest.js
@@ -1,39 +1,27 @@
 const { app } = require('@azure/functions');
-const Busboy = require('busboy');
 
 app.http('techpackRequest', {
-    methods: ['POST'],
+    methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        return new Promise((resolve, reject) => {
-            context.log('Starting...');
-            context.log('Request headers:', request.headers);
-            context.log('Request method:', request.method);
+        context.log(`Http function processed request for url "${request.url}"`);
 
-            const busboy = Busboy({ headers: request.headers });
-            const fields = {};
-            const files = [];
-            context.log('busboy created');
+        const fields = {};
+        const files = {};
 
-            busboy.on('field', (fieldname, val) => {
-                context.log(`Field [${fieldname}]: value: ${val}`);
-                fields[fieldname] = val;
-            });
-
-            busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-                context.log(`File [${fieldname}] received: ${filename}`);
-                file.on('data', (data) => {
-                    context.log(`File [${fieldname}] chunk received (${data.length} bytes)`);
-                });
-                file.on('end', () => {
-                    context.log(`File [${fieldname}] finished`);
-                    files.push({ fieldname, filename, mimetype });
-                });
-            });
-
-            busboy.on('finish', () => {
-                context.log('Form parsing completed');
-            });
+        const formData = await request.formData();
+        formData.entries().forEach(element => {
+            if (fields[element[0]] != null) {
+                fields[element[0]].push(element[1]);
+            } else {
+                fields[element[0]] = [element[1]];
+            }
+            if (element[0]=="companyName") {
+                company_name = element[1];
+            }
         });
+        context.log(fields);
+        
+        return { body: `Hello!` };
     }
 });
